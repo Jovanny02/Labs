c0587124dc997153401574c33d7a0c8be240b6cf
Jovanny02 <32178261+Jovanny02@users.noreply.github.com>
2017-09-26 18:59:40 -0400

    lab

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+    int Rick = 0;
+    int Morty = 1;
+    Scanner scnr = new Scanner(System.in);
+
+    System.out.println("Enter Rick");
+
+    Rick = scnr.nextInt();
+    System.out.println(Rick + Morty);
+
+    return;
+    }
+
+}
